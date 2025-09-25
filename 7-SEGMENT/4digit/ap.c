b537598df7e65aa24df0ba231baf694f1fd32fe4
Youngbin Yoo <97148877+Viiiin94@users.noreply.github.com>
2025-09-25 10:49:56 +0900

    Update ap.c

--- 7-SEGMENT/4digit/ap.c
+++ 7-SEGMENT/4digit/ap.c
@@ -16,7 +16,13 @@ void apMain()
     uint32_t prevTime = 0;
 
     while (1)
-    {
+    {    
+        // count 9999넘으면 0으로 초기화
+        if(count > 9999)
+        {
+            count = 0;
+        }
+        
         FND_Display(count); // FND에 count 값을 출력
 
         if (timeTick - prevTime > 100) // 100ms 지날 때마다 count 값을 1씩 증가
