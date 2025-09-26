9eb324208adf52ca9f2dc1cf3d7ddd39fbe25b6e
Youngbin Yoo <97148877+Viiiin94@users.noreply.github.com>
2025-09-26 15:55:29 +0900

    Create ap.c

---
+++ INTERRUPT/interrupt_uart/ap/ap.c
@@ -0,0 +1,23 @@
+#include "ap.h"
+
+void apInit()
+{
+}
+
+void apMain()
+{
+
+    stdout = &OUTPUT;
+
+    UART_Init();
+    sei();
+
+    while (1)
+    {
+        if (rxFlag == 1)
+        {
+            rxFlag = 0;
+            printf(rxBuff);
+        }
+    }
+}
