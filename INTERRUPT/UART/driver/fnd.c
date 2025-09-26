cdbad5191c91d91d47ac4b65ad8760c917b7a28d
Youngbin Yoo <97148877+Viiiin94@users.noreply.github.com>
2025-09-26 11:34:03 +0900

    Create fnd.c

---
+++ INTERRUPT/UART/driver/fnd.c
@@ -0,0 +1,24 @@
+#include "fnd.h"
+
+void FND_Display(uint8_t data)
+{
+    uint8_t fndData[10] = {
+        0x3f,
+        0x06,
+        0x5b,
+        0x4f,
+        0x66,
+        0x6d,
+        0x7d,
+        0x07,
+        0x7f,
+        0x6f,
+    };
+
+    if (data >= '0' || data <= '9')
+    {
+        uint8_t index = data - '0';
+        FND_DATA_PORTA = fndData[index];
+        FND_DATA_PORTB = fndData[index];
+    }
+}
